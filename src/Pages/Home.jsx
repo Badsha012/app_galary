8197419959a9a2c0df510dab8bdd5b722d19fb3e
Badsha012 <badshasheikh361@gmail.com>
2025-10-08 09:02:25 +0600

    nine commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>I am home Page</h1>
-        </div>
-    );
+  return (
+    <div>
+     <div className="text-center sm:p-4 ">
+         <h1 className=" md:text-5xl sm:text-2xl text-center font-bold text-black  py-1">We Build<br></br> <span className=" text-violet-700"> Productive </span>Apps</h1>
+         <p className="text-gray-400">At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.
+            <br></br>Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+     </div>
+
+    </div>
+  );
 };
 
 export default Home;
